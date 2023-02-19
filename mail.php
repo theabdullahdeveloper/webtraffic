88bfd3d1b3061254abb0217a758feb550fc63e94
mrwebicon <124039493+mrwebicon@users.noreply.github.com>
2023-02-19 17:54:49 +0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,16 +1,23 @@
 <?php
-//get data from form  
+// get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
+
 $to = "abdullahofficial116@gmail.com";
-$subject = "Mail From Web Trafic";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@webtrafic.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+$subject = "Mail From Web Traffic";
+$txt ="Name = ". $name . "\r\nEmail = " . $email . "\r\nMessage =" . $message;
+$headers = "From: noreply@webtraffic.com" . "\r\n" . "CC: somebodyelse@example.com";
+
+// send email and check for errors
+if ($email != NULL) {
+    if (mail($to, $subject, $txt, $headers)) {
+        echo "Email sent successfully.";
+    } else {
+        echo "Error: Email was not sent.";
+    }
 }
-//redirect
+
+// redirect
 header("Location:index.html");
 ?>
