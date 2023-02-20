b584e7e8b732a903bd1d4c2111bec45ef8c71c6e
mrwebicon <124039493+mrwebicon@users.noreply.github.com>
2023-02-20 17:48:02 +0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,23 +1,49 @@
-<?php
-// get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const app = express();
 
-$to = "abdullahofficial116@gmail.com";
-$subject = "Mail From Web Traffic";
-$txt ="Name = ". $name . "\r\nEmail = " . $email . "\r\nMessage =" . $message;
-$headers = "From: noreply@webtraffic.com" . "\r\n" . "CC: somebodyelse@example.com";
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// send email and check for errors
-if ($email != NULL) {
-    if (mail($to, $subject, $txt, $headers)) {
-        echo "Email sent successfully.";
-    } else {
-        echo "Error: Email was not sent.";
+// POST route to handle form submission
+app.post('/contact', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  // Create transporter object
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'your_email@gmail.com', // Replace with your email address
+      pass: 'your_email_password' // Replace with your email password
     }
-}
+  });
 
-// redirect
-header("Location:index.html");
-?>
+  // Create email message
+  const mailOptions = {
+    from: email,
+    to: 'your_email@gmail.com', // Replace with your email address
+    subject: 'New Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`
+  };
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error: Unable to send email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
